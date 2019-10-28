6620dc9c72a8d268970c2d00a099a5841046e18e
Costi <comisarulmoldovan@gmail.com>
2019-10-27 23:38:55 -0500

    Magnetic Mushroom sensor

---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+#define MAGNET_AWAY 1
+#define MOVING_AVG 1000
+
+class MagnetSensor {
+    int currentStateIndex;
+    bool states[MOVING_AVG];
+    bool state;
+    int8_t pin;
+  public:
+    MagnetSensor(int somePin);
+    int getState();
+    void setup(); // arduino setup
+  private:
+    bool calcAvg();
+    int read();
+};
+
+MagnetSensor::MagnetSensor(int somePin){
+  pin = somePin;
+}
+
+void MagnetSensor::setup(){
+  pinMode(pin, INPUT_PULLUP);
+}
+
+int MagnetSensor::read(){
+  return digitalRead(pin);
+}
+
+// like elections, if 50% of booleans are one direction, that's the average
+bool MagnetSensor::calcAvg(){
+  int sum = 0;
+  for(int i=0; i<MOVING_AVG; i++){
+    //Serial.print(arr[i]);
+    //Serial.print(" ");
+    sum += states[i];
+  }
+  //Serial.println("");
+  return (sum > MOVING_AVG / 2);
+}
+
+int MagnetSensor::getState() {
+  states[currentStateIndex] = read();
+  currentStateIndex++;
+  if(currentStateIndex >= MOVING_AVG){
+    currentStateIndex = 0;
+  }
+  int newState = calcAvg();
+  if(newState != state){
+    Serial.println("Change detected!!!");
+    Serial.print("Mushroom state: ");
+    if(newState == MAGNET_AWAY){
+      Serial.println("Away");
+    } else {
+      Serial.println("On sensor");
+    }
+    state = newState;
+  }
+  return state;
+}
+
+MagnetSensor mushroom(21);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting!");
+  mushroom.setup();
+}
+
+
+void loop() {
+  mushroom.getState();
+}
